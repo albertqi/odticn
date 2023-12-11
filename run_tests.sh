54d247434033a6a9b0285fa68f729a37ed0c7ba6
Ronak Malik <ronakmalik4242@gmail.com>
2023-12-10 23:23:53 -0500

    Add tee's to run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 echo "All Reduce"
-make all_reduce
+make all_reduce | tee all_reduce.txt
 
 killall python3
 
 echo "All Reduce Decreased 2"
-make all_reduce_decreased
+make all_reduce_decreased | tee all_reduce_decreased_2.txt
 
 killall python3
 
 echo "All Reduce Decreased 4"
-make all_reduce_decreased4
+make all_reduce_decreased4 | tee all_reduce_decreased_4.txt
 
 killall python3
 
 echo "All Reduce Decreased 8"
-make all_reduce_decreased8
+make all_reduce_decreased8 | tee all_reduce_decreased_8.txt
 
 killall python3
 
 echo "Gossip Learning Random"
-make gossip_learning_random
+make gossip_learning_random | tee gossip_learning_random.txt
 
 killall python3
 
 echo "Gossip Learning Latency"
-make gossip_learning_latency
+make gossip_learning_latency | tee gossip_learning_latency.txt
+
